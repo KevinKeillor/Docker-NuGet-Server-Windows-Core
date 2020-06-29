c9bb0c5f2ac7912cb31ef2c0f738b6f14969760b
Jamie Bowman <mrjamiebowman@gmail.com>
2020-06-28 22:38:51 -0400

    updates

--- nugetserver/run.ps1
+++ nugetserver/run.ps1
@@ -1,7 +1,7 @@
 
 docker rm -f nugetserver
 
-docker run -d --name nugetserver -p 80:80 -p 443:443 mrjb/nugetserver
+docker run -d --name nugetserver -v packages:C:\inetpub\wwwroot\Packages -p 80:80 -p 443:443 mrjb/nugetserver
 docker inspect -f "{{ .NetworkSettings.Networks.nat.IPAddress }}" nugetserver
 docker ps -a
-#docker exec -it nugetserver powershell
+docker exec -it nugetserver powershell
