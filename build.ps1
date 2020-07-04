0a7a3d952f269dcd43a2c03a11bf6001f4046e3e
Jamie Bowman <mrjamiebowman@gmail.com>
2020-07-03 20:32:44 -0400

    updates

--- build.ps1
+++ build.ps1
@@ -1,12 +1,2 @@
 Write-Host "Building NuGet Server image..."
-
-# empty directory
-Get-ChildItem -Path ${PWD}\source -Include *.* -File -Recurse | foreach { $_.Delete()}
-Get-ChildItem -Path ${PWD}\published -Include *.* -File -Recurse | foreach { $_.Delete()}
-
-
-# clone nuget server into source folder
-git clone https://github.com/NuGet/NuGet.Server.git source
-
-
-docker build -t mrjamiebowman/nugetserver .
+docker build -t mrjamiebowman/nuget-server-windows-server-core .
--- run.ps1
+++ run.ps1
@@ -1,7 +1,8 @@
 # clean up if previous nugetserver is running...
 docker rm -f nugetserver
 
-docker run -d --name nugetserver -v packages:C:\inetpub\wwwroot\Packages -p 80:80 -p 443:443 mrjamiebowman/nugetserver
+docker run -d --name nugetserver -v ${PWD}\packages:C:\inetpub\wwwroot\Packages -p 80:80 -p 443:443 mrjamiebowman/nugetserver
 docker inspect -f "{{ .NetworkSettings.Networks.nat.IPAddress }}" nugetserver
 docker ps -a
-docker exec -it nugetserver powershell
+
+# docker exec -it nugetserver powershell
